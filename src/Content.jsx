95774de44bda5636c9532b3ec8e55abfd0379108
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-11-07 20:32:59 -0500

    changes Content to import useState and changes variable to see content in page

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { PostNew } from "./PostNew";
 import { PostsIndex } from "./PostsIndex";
+import { useState } from "react";
 
 export function Content() {
-  let posts = [];
+  const [posts, setPosts] = useState([]);
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
       console.log(response.data);
-      posts = response.data;
+      setPosts(response.data);
     });
   };
   return (
