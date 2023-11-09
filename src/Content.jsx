02aac3be3afc1fbb6a6a61f85b920c09db5a8c1c
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-11-08 22:01:49 -0500

    adds show button and clos tab

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,9 +3,11 @@ import { PostNew } from "./PostNew";
 import { PostsIndex } from "./PostsIndex";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Modal } from "./Modal";
 
 export function Content() {
   const [posts, setPosts] = useState([]);
+  const [isPostShowVisible, setIsPostShowVisible] = useState(false);
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
       console.log(response.data);
@@ -14,10 +16,21 @@ export function Content() {
   };
   useEffect(handleIndexPosts, []);
 
+  const handleShowPosts = () => {
+    setIsPostShowVisible(true);
+  };
+
+  const handleClose = () => {
+    setIsPostShowVisible(false);
+  };
+
   return (
     <div className="main">
       <PostNew />
-      <PostsIndex myPosts={posts} />
+      <PostsIndex myPosts={posts} onShowPost={handleShowPosts} />
+      <Modal show={isPostShowVisible} onClose={handleClose}>
+        <p> a blog: </p>
+      </Modal>
     </div>
   );
 }
