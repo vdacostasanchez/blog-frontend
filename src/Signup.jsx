d067a788792034ed266c4661e484ae3696fd4108
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-03 10:27:41 -0500

    adds html attribute to prevent more than 20 characters being entered in name in signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export function Signup() {
   const [errors, setErrors] = useState([]);
+  const [name, setName] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -31,7 +32,17 @@ export function Signup() {
       </ul>
       <form onSubmit={handleSubmit}>
         <div>
-          Name: <input name="name" type="text" />
+          Name:{" "}
+          <input
+            name="name"
+            type="text"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            maxLength={20}
+          />
+          <small>{20 - name.length} characters remaining</small>
         </div>
         <div>
           Email: <input name="email" type="email" />
